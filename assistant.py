23af85872849be73b27a90d840b5fa12228d4678
moeenDehqan <moeen.dehqan@gmail.com>
2022-06-06 10:42:08 +0430

    sandoq

--- assistant.py
+++ assistant.py
@@ -1,7 +1,41 @@
+from msilib import type_binary
 import pandas as pd
 from io import StringIO
 import requests
+import pymongo
+import numpy as np
+from selenium import webdriver
+import time
 
+client = pymongo.MongoClient("localhost", 27017)
+eftManege = client['eftManege']
+
+
+
+def getNev(typen,url):
+    if typen=='A':
+        driver = webdriver.Chrome(executable_path='chromedriver.exe')
+        driver.get(url)
+        nevEbtal = ''
+        dateNev = ''
+        while nevEbtal == '' or dateNev == '':
+            nevEbtal = driver.find_element_by_css_selector('#PRedTran').text
+            dateNev = driver.find_element_by_css_selector('#NAVDate').text
+            time.sleep(2)
+
+        nevEbtal = int(nevEbtal.replace(',',''))
+        dateNev = dateNev.split(' ')[0].split('/')
+        if len(dateNev[1])==1 and len(dateNev[2])==1:
+            dateNev = dateNev[0] + '0' + dateNev[1] + '0' + dateNev[2]
+        elif len(dateNev[1])==1 and len(dateNev[2])>1:
+            dateNev = dateNev[0] + '0' + dateNev[1] + dateNev[2]
+        elif len(dateNev[1])>=1 and len(dateNev[2])==1:
+            dateNev = dateNev[0] + dateNev[1] + '0' + dateNev[2]
+        else:
+            dateNev = dateNev[0] +  dateNev[1] +  dateNev[2]
+        dateNev = int(dateNev)
+
+        return [nevEbtal, dateNev]
 
 def gregorian_to_jalali(GDate):
     GDate = str(GDate)
@@ -37,11 +71,7 @@ def gregorian_to_jalali(GDate):
 
 
 def histori_Update():
-    dff = pd.read_excel('Data/histori.xlsx').drop(columns='Unnamed: 0')
-    Last_update = dff['<DTYYYYMMDD>'].max()
-    if Last_update>15000000:
-        dff['<DTYYYYMMDD>'] = [gregorian_to_jalali(x) for x in dff['<DTYYYYMMDD>']]
-    dff = dff[dff['<DTYYYYMMDD>']<dff['<DTYYYYMMDD>'].max()]
+    dff = pd.DataFrame(khatamDb.find_one(sort=[("<DTYYYYMMDD>", pymongo.DESCENDING)]))
     Last_update = dff['<DTYYYYMMDD>'].max()
     df = StringIO(requests.get(url='http://www.tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i=18865325633315847').text)
     df = pd.read_csv(df,sep=",")
@@ -51,3 +81,59 @@ def histori_Update():
         dff = dff.append(df)
 
     dff.to_excel('Data/histori.xlsx')
+
+#این تابع صرفا برای ایجاد صندوق جدید مورد استفاده قرار میگیرد
+def newEtf(urlStart,name,urlNev,TypeSite):
+    '''
+    urlStart: لینک تابلو tse
+    name: نماد صندوق اختصار
+    urlNev: ادرسی که nev ابطال به صورت دقیق درج شده باشد
+    TypeSite: اگر nev ابطال در tse باشد نوع A
+    '''
+    infoEtfDb =  eftManege['infoEtfDb']
+    try:already = len(dict(infoEtfDb.find_one({'name':name})))==0
+    except:already = True
+    if already:
+        print('create Etf')
+        infoEtfDb.insert_one({'name':name, 'url':urlStart, 'urlNev':urlNev, 'site':TypeSite})
+        df = StringIO(requests.get(url=urlStart).text)
+        df = pd.read_csv(df,sep=",")
+        df['<DTYYYYMMDD>'] = df['<DTYYYYMMDD>'] = [gregorian_to_jalali(x) for x in df['<DTYYYYMMDD>']]
+        df['nav'] = np.nan
+        df = df.to_dict(orient='records')
+        newEtfDb = eftManege[name]
+        newEtfDb.insert_many(df)
+        print('set Etf')
+    else:
+        print('STOP, Etf is already')
+
+def updateEtf(name):
+    EtfDb = eftManege[name]
+    lastUpdate = EtfDb.find_one(sort=[('<DTYYYYMMDD>', pymongo.DESCENDING)])['<DTYYYYMMDD>']
+    etfinfo = eftManege['infoEtfDb'].find_one({'name':name})
+    url = etfinfo['url']
+    typn = etfinfo['site']
+    urlnev = etfinfo['urlNev']
+    df = pd.read_csv(StringIO(requests.get(url=url).text),sep=",")
+    df['<DTYYYYMMDD>'] = [gregorian_to_jalali(x) for x in df['<DTYYYYMMDD>']]
+    df = df[df['<DTYYYYMMDD>']>lastUpdate]
+
+    if len(df)>0:
+        nav = getNev(typn,urlnev)
+        print(nav)
+        df['nav'] = np.nan
+        df = df.set_index('<DTYYYYMMDD>')
+        df['nav'][nav[1]] = nav[0]
+        print(df)
+        df = df.reset_index()
+        df = df.to_dict(orient='records')
+        EtfDb.insert_many(df)
+        print('Update')
+    else:
+        print('Updated')
+
+def listEtf():
+    listEtf = []
+    for x in eftManege['infoEtfDb'].find():
+        listEtf.append(x['name'])
+    return listEtf
--- pipeData.py
+++ pipeData.py
@@ -1,16 +1,14 @@
 
-from selenium import webdriver
-from io import StringIO
-import requests
-import pandas as pd
-import assistant as ast
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
-
-driver.get('http://etf.isatispm.com/')
-nevEbtal = driver.find_element_by_css_selector('#Live_SellNAVPerShare').text
-nevEbtal = int(nevEbtal.split(' ')[0].replace(',',''))
-date = driver.find_element_by_css_selector('#Live_JalaliDate').text
-date = int(date.split(' ')[1].replace('/',''))
+import time
+import assistant as ass
 
 
-print(df)
+
+while True:
+    h = time.localtime().tm_hour
+    m = time.localtime().tm_min
+    if m == 00 and h ==16:
+        listEtf = ass.listEtf()
+        for x in listEtf:
+            ass.updateEtf(x)
+        time.sleep(120)
